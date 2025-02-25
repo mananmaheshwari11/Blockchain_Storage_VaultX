b06ec1377f6d1778936bed8db173b184a2555cc4
mananmaheshwari11 <maheshwarimanan2211@gmail.com>
2025-02-25 18:20:18 +0530

    Creating routes in frontend for navigation

--- certificate/src/App.jsx
+++ certificate/src/App.jsx
@@ -10,6 +10,9 @@ import NotFound from "./pages/NotFound"
 import { Toaster } from "react-hot-toast"
 import Entities from "./pages/Entities"
 import AboutVaultX from "./pages/About"
+import HelpPage from "./pages/Help"
+import Info from "./pages/Info"
+import UpdateForm from "./pages/UpdateForm"
 
 function App() {
 
@@ -21,12 +24,15 @@ function App() {
     <Route path="/signup" element={<Signup/>}/>
     <Route path="/signin" element={<Signin/>}/>
     <Route path="/about" element={<AboutVaultX/>}/>
+    <Route path="/help" element={<HelpPage/>}/>
     <Route path="*" element={<NotFound/>}/>
     <Route path="vault" element={<AccessContext/>}>
     <Route path="/vault" element={<Homepage/>}/>
+    <Route path="info" element={<Info/>}/>
     <Route path="org" element={<AdminAccess/>}>
     <Route path="entities" element={<Entities/>}/>
     <Route path="upload" element={<UploadForm/>}/>
+    <Route path="update" element={<UpdateForm/>}/>
     </Route>
     <Route path="retrieve" element={<FormRetrieve/>}/>
     </Route>
