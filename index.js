0002c81a019d6b9819455d714ccad2717143423c
mananmaheshwari11 <maheshwarimanan2211@gmail.com>
2025-02-25 23:44:41 +0530

    Creating static file in index.js and build scripts

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv"
 import connectDb from "./config/db.js";
 import universityRoutes from "./routes/universityRoutes.js"
 import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
+import path from 'path';
 
 dotenv.config()
 connectDb();
@@ -15,6 +17,34 @@ app.use(cors({credentials:true}))
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(express.json())
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'certificate/dist')));
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "certificate/dist", "index.html"));
+  });
+app.get("/help", (req, res) => {
+    res.sendFile(path.join(__dirname, "certificate/dist", "index.html"));
+  });
+app.get("/vault/org/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "certificate/dist", "index.html"));
+  });
+app.get("/vault/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "certificate/dist", "index.html"));
+  });
+app.get("/vault", (req, res) => {
+    res.sendFile(path.join(__dirname, "certificate/dist", "index.html"));
+  });
+app.get("/signin", (req, res) => {
+    res.sendFile(path.join(__dirname, "certificate/dist", "index.html"));
+  });
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "certificate/dist", "index.html"));
+  });
 
 app.use('/api/auth',universityRoutes);
 
